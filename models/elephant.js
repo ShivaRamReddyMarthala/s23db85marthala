b2d95c8af40bacc395edc03da134fde7cd8c3cff
ShivaRamReddyMarthala <142836346+ShivaRamReddyMarthala@users.noreply.github.com>
2023-11-25 10:25:21 -0600

    commit changes bonus

--- models/elephant.js
+++ models/elephant.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose")
 const elephantSchema = mongoose.Schema({
-name: String,
-species: String,
+name:{
+    type : String,
+    require : true,
+    minlength : 10,
+    maxlength : 50,
+} ,
 age:{
     type:Number,
     min : 5,
     max : 50,
     require : true
-}
-
+},
+species: String,
 })
 module.exports = mongoose.model("elephant", elephantSchema)
