4e5f9b72d7989d729499def6d65adcb877ef1c99
ShivaRamReddyMarthala <142836346+ShivaRamReddyMarthala@users.noreply.github.com>
2023-11-24 18:08:19 -0600

    commit changes part 5 and 6

--- models/elephant.js
+++ models/elephant.js
@@ -1,7 +1,13 @@
 const mongoose = require("mongoose")
 const elephantSchema = mongoose.Schema({
 name: String,
-age: Number,
-species: String
+species: String,
+age:{
+    type:Number,
+    min : 5,
+    max : 50,
+    require : true
+}
+
 })
 module.exports = mongoose.model("elephant", elephantSchema)
--- routes/elephant.js
+++ routes/elephant.js
@@ -23,16 +23,16 @@ router.get('/', elephant_controlers.elephant_view_all_Page );
 
 
 /* GET detail elephant page */
-router.get('/detail', elephant_controlers.elephant_view_one_Page);
+router.get('/detail', secured, elephant_controlers.elephant_view_one_Page);
 
 /* GET create elephant page */
-router.get('/create', elephant_controlers.elephant_create_Page);
+router.get('/create', secured, elephant_controlers.elephant_create_Page);
 
 /* GET create update page */
 router.get('/update', secured, elephant_controlers.elephant_update_Page);
 
 /* GET delete elephant page */
-router.get('/delete', elephant_controlers.elephant_delete_Page);
+router.get('/delete', secured, elephant_controlers.elephant_delete_Page);
 
 
 module.exports = router;
